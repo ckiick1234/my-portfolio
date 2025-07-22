22ff06bf75cbf2472de3dbced8b415dcbbcae3cb
ckiick1234 <ckiick1234@gmail.com>
2025-07-22 14:50:02 -0700

    environment

--- src/app/text-function.service.ts
+++ src/app/text-function.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TextFunctionService {
 
-  private readonly apiUrl = 'http://localhost:7129/api/httpget';
+  private readonly apiUrl = environment.apiUrl;//'http://localhost:7129/api/httpget';
 
   constructor(private http: HttpClient) { }
 
   getText(): Observable<string> {
-    return this.http.get(this.apiUrl, { responseType: 'text' });
+    return this.http.get(this.apiUrl + '/api/httpget', { responseType: 'text' });
   }
 }
